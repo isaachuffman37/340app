a27bbe923ecc5e6f7ddab0260c26cd525af9c3c9
Isaac Huffman <122467839+isaachuffman37@users.noreply.github.com>
2024-11-18 09:03:50 -0700

    made the database

---
+++ database/assignment2.sql
@@ -0,0 +1,33 @@
+INSERT INTO public.account 
+(account_firstname, 
+account_lastname,
+account_email,
+account_password) 
+VALUES	(
+	'Tony',
+	'Stark',
+	'tony@starkent.com',
+	'Iam1ronM@n'
+);
+
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+DELETE FROM public.account
+WHERE account_id = 1;
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+
+SELECT inventory.inv_make, inventory.inv_model, classification.classification_name
+FROM inventory
+INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
+
+UPDATE public.inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
