6b10d28371b8f70aa11adac2b8c4e28c097cfab3
Isaac Huffman <122467839+isaachuffman37@users.noreply.github.com>
2024-11-28 09:16:57 -0700

    made the data validation and password hashing

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -9,6 +9,14 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/registration", utilities.handleErrors(accountController.buildRegister));
 router.post('/register', regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
 
 
 module.exports = router;
